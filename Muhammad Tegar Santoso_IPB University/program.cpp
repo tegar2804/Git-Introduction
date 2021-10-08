492183ca109c508b5469532e3d5893d72551c3e9
Muhammad Tegar Santoso <muhammadtegar2804@gmail.com>
2021-10-08 21:42:00 +0700

    First submission by Muhammad Tegar Santoso

---
+++ Muhammad Tegar Santoso_IPB University/program.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Nama : Muhammad Tegar Santoso" << endl;
+    cout << "Jurusan : Ilmu Komputer" << endl;
+    cout << "Alasan ingin masuk GDSC : Saya ingin meningkatkan kemampuan saya terutama dalam dunia sains dan teknologi" << endl;
+
+    return 0;
+}
